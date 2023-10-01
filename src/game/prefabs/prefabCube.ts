6e8375367b4a58536ab33c0584c726b4569058e6
Dean R <platoisalive@outlook.com>
2023-10-01 11:55:53 +0000

    centered cube

--- src/game/prefabs/prefabCube.ts
+++ src/game/prefabs/prefabCube.ts
@@ -5,12 +5,12 @@ import { Rotate } from "../components/Rotate";
 
 export const prefabCube: IPrefab = {
   name: "cube",
-  position: new BABYLON.Vector3(-4, 0, 10),
+  position: new BABYLON.Vector3(0, 0, 10),
   children: [
     {
       name: "cube",
       model: modelBlock,
-      position: new BABYLON.Vector3(2, 0, 0),
+      position: new BABYLON.Vector3(0, 0, 0),
       postBuild: (n) => new Rotate(n),
       children: [
         {
